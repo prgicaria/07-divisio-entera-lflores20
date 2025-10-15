7d597fd76037a6097b8b5c430f929b32caf6a6e3
lorenzo <lflores@instituticaria.cat>
2025-10-15 10:32:30 +0200

    divisió feta

---
+++ divisio.py
@@ -0,0 +1,5 @@
+dividend = int(input("posa el teu dividend: "))
+divisor = int(input("posa el teu divisor: "))
+quocient = dividend / divisor
+residu = dividend % divisor
+print ("El quocient de la divisió",dividend, "entre", divisor, "és", quocient, "i el residu és", residu)
