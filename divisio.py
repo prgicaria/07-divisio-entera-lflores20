7d423334cb245282b1abaab746eef6306a5f222a
lorenzo <lflores@instituticaria.cat>
2025-10-17 10:53:21 +0200

    corregit

--- divisio.py
+++ divisio.py
@@ -1,5 +1,6 @@
 dividend = int(input("posa el teu dividend: "))
 divisor = int(input("posa el teu divisor: "))
-quocient = dividend / divisor
+quocient = dividend // divisor
 residu = dividend % divisor
-print ("El quocient de la divisió",dividend, "entre", divisor, "és", quocient, "i el residu és", residu)
+print("El quocient de la divisió", dividend, "entre", divisor, "és", quocient,
+      "i el residu és", residu)
